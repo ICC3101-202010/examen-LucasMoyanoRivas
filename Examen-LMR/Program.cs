974b306a4c3e7803e9f282ff04f96bb793078de2
Lucas Moyano Rivas <lucasmoyanor@Lucass-MacBook-Pro-2.local>
2020-07-02 13:49:58 -0400

    primer commit ready

---
+++ Examen-LMR/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExamenLMR
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
